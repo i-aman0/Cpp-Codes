dd8c409a6caddcf62e14b4ceb880b187a2115302
Aman Khan <aman@Amans-MacBook-Air.local>
2022-08-22 00:16:30 +0530

    Commit to GitHub

--- Leetcode/PascalsTriangle.cpp
+++ Leetcode/PascalsTriangle.cpp
@@ -3,30 +3,36 @@
 using namespace std;
 
 vector<vector<int> > generateTriangle(int n){
-    int dp[31][31];
-    memset(dp, 0, sizeof dp);
+    int arr[30][30];
+
+    // setting every element of the 2D array to '0'
+    // the memset function copies a single character for a specified numebr of times to an object
+    // here it copies '0' for sizeof number of times to the 2D array
+    memset(arr, 0, sizeof arr);
 
     //setting first column and diagonal elements to "1"
     for(int i=0; i<n; i++){
         // first column
-        dp[i][0]=1;
+        arr[i][0]=1;
 
         // diagonal element 
-        dp[i][i]=1;
+        arr[i][i]=1;
     }
 
     for(int i=2;i<n;i++){
         for(int j=1;j<n;j++){
             // the current element is the sum of two numbers which are directly above it
-            dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
+            arr[i][j]=arr[i-1][j-1]+arr[i-1][j];
         }
     }
 
     vector<vector<int> > ans;
     for(int i=0;i<n;i++){
+
+        // a vector for storing the elements present in a single row of the 2D array arr
         vector<int> temp;
         for(int j=0;j<i+1;j++){
-            temp.push_back(dp[i][j]);
+            temp.push_back(arr[i][j]);
         }
         ans.push_back(temp);
     }
