6e53f0d1c4ca906ac9ae31222812447ad7c8252d
i-aman0 <khan.aman902688@gmail.com>
2022-10-28 23:15:50 +0530

    Binary Search

---
+++ Recursion/BinarySearch.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+
+bool solve(int* arr, int start, int end, int key){
+    int mid=(start+end)/2;
+
+    if(start>end){
+        return false;
+    }
+
+    if(arr[mid]==key){
+        return true;
+    }
+    else if(arr[mid]<key){
+        return solve(arr, mid+1, end, key);
+    }
+    else{
+        return solve(arr, start, mid-1, key);
+    }
+}
+
+bool binarySearch(int* arr, int size, int key){
+    return solve(arr, 0, size-1, key);
+}
+
+int main()
+{
+    int arr[]={3,5,7,8,23};
+    int size=5;
+    int key;
+    cout<<"Enter the number you want to search"<<endl;
+    cin>>key;
+
+    if(binarySearch(arr, size, key)){
+        cout<<"The key is present in the array"<<endl;
+    }
+    else{
+        cout<<"The key is not present in the array"<<endl;
+    }
+
+    return 0;
+}
