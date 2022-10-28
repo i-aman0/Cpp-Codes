d13ab7f3c022291a8498bd1f6f1f0293e3a63c4d
Aman Khan <aman@Amans-MacBook-Air-2.local>
2022-10-28 09:38:38 +0530

    Testing

--- Recursion/IsSortedArray.cpp
+++ Recursion/IsSortedArray.cpp
@@ -9,7 +9,6 @@ bool isSorted(int* arr, int size){
     }
 
     // checking if first and second element of the array are not sorted
-    // if not return false
     if(arr[0]>arr[1]){
         return false;
     }
