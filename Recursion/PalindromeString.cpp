c2e64b94417adc1c98184ea130321bdcbffa4c64
i-aman0 <khan.aman902688@gmail.com>
2022-10-29 18:47:22 +0530

    New method added

--- Recursion/PalindromeString.cpp
+++ Recursion/PalindromeString.cpp
@@ -1,35 +1,49 @@
 #include<iostream>
 using namespace std;
 
-void reverseString(string &str, int i, int j){
-    // base case
+// 1st approach by calculating the reverse first
+
+// void reverseString(string &str, int i, int j){
+//     // base case
+//     if(i>j){
+//         return;
+//     }
+
+//     swap(str[i], str[j]);
+//     i++;
+//     j--;
+
+//     reverseString(str, i, j);
+// }
+
+// bool isPalindrome(string str){
+//     string temp=str;
+//     reverseString(temp, 0, temp.length()-1);
+//     if(temp==str){
+//         return true;
+//     }
+//     else{
+//         return false;
+//     }
+// }
+
+bool isPalindrome(string str, int i, int j){
     if(i>j){
-        return;
-    }
-
-    swap(str[i], str[j]);
-    i++;
-    j--;
-
-    reverseString(str, i, j);
-}
-
-bool isPalindrome(string str){
-    string temp=str;
-    reverseString(temp, 0, temp.length()-1);
-    if(temp==str){
         return true;
     }
-    else{
+    if(str[i]!=str[j]){
         return false;
+    }
+    else{
+        return isPalindrome(str, i+1, j-1);
     }
 }
 
 int main()
 {
-    string str="aabaa";
+    string str="aabbaaa";
     
-    bool ans=isPalindrome(str);
+    bool ans=isPalindrome(str, 0, str.length()-1);
 
     if(ans){
         cout<<"The string is palindrome"<<endl;
