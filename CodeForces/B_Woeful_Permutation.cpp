2bf9e97c79dbd13e45efd7b7281e35b732883c69
i-aman0 <khan.aman902688@gmail.com>
2024-08-28 19:00:15 +0530

    Added code for woeful permutation in codeforces

--- CodeForces/B_Woeful_Permutation.cpp
+++ CodeForces/B_Woeful_Permutation.cpp
@@ -7,6 +7,8 @@
 
 using namespace std;
 
+// https://codeforces.com/problemset/problem/1712/B
+
 
 int main(){
     int t;
@@ -16,10 +18,40 @@ int main(){
         ll n;
         cin >> n;
 
-        vector<ll> ans(n);
+        vector<ll> ans;
+
+        if(n % 2){
+            ans.push_back(1);
+
+            for(int i = 1; i < n; i += 2){
+                ans.push_back(i + 2);
+                ans.push_back(i + 1);
+            }
+        }
+        else{
+            for(int i = 0; i < n; i += 2){
+                ans.push_back(i + 2);
+                ans.push_back(i + 1);
+            }
+        }
+
+        for(int i = 0; i < n; i++){
+            cout << ans[i] << " ";
+        }
+
+        cout << endl;
 
         
     }
 
     return 0;
 }
+
+
+/*
+    if n is odd (n % 2) e.g n == 5
+    1, 3, 2, 5, 4
+    if n is even e.g. n == 6
+    2, 1, 4, 3, 6, 5
+
+*/
